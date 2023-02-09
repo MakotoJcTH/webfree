2a8a9f21dfc07c412af79c8e14a48c51150284d9
warayus srinim <warayus_sr65@live.rmutl.ac.th>
2023-02-09 12:05:28 +0700

    update webfree 1.1.1

--- index.php
+++ index.php
@@ -1,18 +1,131 @@
 <!doctype html>
 <html lang="en">
 
-  <?php include("./components/head.php"); ?>
+<?php include("./components/head.php"); ?>
+<?= $img ="https://chillchilljapan.com/wp-content/uploads/2021/05/179446704_452734805816015_8459639923628099990_n.jpg" ?>
 
-  <body>
+<body>
 
     <?php include("./components/nav.php"); ?>
 
+    
+
     <div class="container-fluid mt-3">
-      <h1>หน้าหลัก</h1>
-      <p>ระบบฐานข้อมุลนักศึกษา</p> 
-    </div>
+        <h1>หน้าหลัก</h1>
+        <div class="row mt-3">
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?=$img?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the
+                            card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?=$img?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                            of
+                            the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?=$img?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                            of
+                            the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    <?php include("./components/script.php"); ?>
+        <div class="row mt-3">
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?=$img?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the
+                            bulk
+                            of
+                            the
+                            card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?=$img?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up
+                            the
+                            bulk
+                            of
+                            the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?=$img?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up
+                            the
+                            bulk
+                            of
+                            the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-  </body>
+        <div class="row mt-3">
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?=$img?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make
+                            up the
+                            bulk of
+                            the
+                            card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?=$img?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and
+                            make up
+                            the bulk
+                            of
+                            the card's content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?=$img?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and
+                            make up
+                            the bulk
+                            of
+                            the card's content.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <?php include("./components/script.php"); ?>
+
+</body>
+
 </html>
